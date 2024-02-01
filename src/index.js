81fa7ba1fab6a5b5fe29e80632f51a416bbf592d
marianacadete <135378075+marianacadete@users.noreply.github.com>
2024-02-01 17:27:39 -0300

    Adicionado a opção de inserir todo e editar

--- src/index.js
+++ src/index.js
@@ -12,7 +12,9 @@ const app = express()
 
 app.use(express.json())
 
-app.get('/',  (req,res) => {console.log(process.env)})
+//app.get('/',  (req,res) => {console.log(process.env)})
+
+// consultar 'users' 
 app.get('/users', async(req,res) => {
     try {
         const {rows} = await pool.query('select * from users')
@@ -23,6 +25,7 @@ app.get('/users', async(req,res) => {
 })
 
 
+// Inserir  'users' 
 app.post('/session', async (req, res) => {
     const { username, age, email } = req.body;
     try {  
@@ -44,6 +47,70 @@ app.post('/session', async (req, res) => {
 });
 
 
+// Consultar 'todos'
+app.get('/todo/:user_id?', async (req, res) => {
+    try {
+        // Se user_id for fornecido na URL, use-o; caso contrário, deixe user_id como undefined
+        const user_id = req.params.user_id;
+
+        let query;
+        let queryParams;
+
+        if (user_id) {
+            // Se user_id foi fornecido, inclua na consulta SQL
+            query = 'SELECT * FROM todos WHERE user_id = ($1)';
+            queryParams = [user_id];
+        } else {
+            // Se user_id não foi fornecido, consulte todos os registros
+            query = 'SELECT * FROM todos';
+            queryParams = [];
+        }
+        
+        const allTodos = await pool.query(query, queryParams);
+        return res.status(200).send(allTodos.rows);
+    } catch (err) {
+        return res.status(400).send(err);
+    }
+});
+
+
+
+// Inserir novo 'todo' 
+app.post('/todo', async (req,res ) => {
+    const {user_id, todo_description ,todo_done} = req.body
+    try{
+        //verificar se o user digitado existe na tabela
+        const verifyUser = await pool.query('SELECT * FROM users WHERE id = $1', [user_id]);
+
+        if (verifyUser.rows.length < 1) {
+            return res.status(400).send("Não existe usuário com esse ID.");
+        }
+
+        //Inserir novo user 
+        const inserirTodo = await pool.query('INSERT INTO todos (user_id, todo_description ,todo_done  ) VALUES  ($1, $2, $3) RETURNING *', [user_id, todo_description ,todo_done])
+        return res.status(200).send(inserirTodo.rows)
+    } catch (err) {
+        return res.status(400).send(err)
+    }
+});
+
+
+
+
+
+
+
+// Alterar  'todo' 
+app.put('/todo/:todo_id', async (req,res) => {
+    const {todo_id} = req.params
+    const data = req.body
+    try{
+        const alterTodo = await pool.query( 'UPDATE todos SET todo_done = true where todo_id = ($1)  RETURNING *', [todo_id])
+        return res.status(200).send(alterTodo.rows)
+        } catch (err) {
+            return res.status(400).send(err)
+        }
+} )
 
 
 
